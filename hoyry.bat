e40818fc24fefe63a15d8fdefa4a09eedf92f599
pelaajahacks <62875333+pelaajahacks@users.noreply.github.com>
2024-02-28 10:50:32 +0200

    Update hoyry.bat

--- hoyry.bat
+++ hoyry.bat
@@ -1,11 +1,6 @@
 @echo off
-:main
-set awd=
-set /p "awd=%cd%>"
-%awd%
 mkdir Hoyry
 cd hoyry
 curl https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe -o s.exe
-curl https://github.com/pelaajahacks/Hoyry/raw/main/7za.exe -o 7za
-7za x s.exe Steam
-goto main
+curl -LJO https://github.com/pelaajahacks/Hoyry/raw/main/7za.exe -o 7za.exe
+7za.exe x s.exe Steam
