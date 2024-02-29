ef9cd029623046628fe162e7ecca91d57642e627
pelaajahacks <62875333+pelaajahacks@users.noreply.github.com>
2024-02-29 15:32:01 +0200

    ny hoyryää

--- hoyry.bat
+++ hoyry.bat
@@ -2,9 +2,7 @@
 mkdir Hoyry
 cd Hoyry
 curl https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe -o s.exe
-curl -LJO https://github.com/pelaajahacks/Hoyry/raw/main/7z.exe -o 7.exe
-7.exe x s.exe Steam
-del 7.exe
-del s.exe
-cd Steam
+curl -LJO https://github.com/pelaajahacks/Hoyry/raw/main/7z.exe
+curl -LJO https://github.com/pelaajahacks/Hoyry/raw/main/7z.dll
+7z.exe x s.exe
 Steam.exe
